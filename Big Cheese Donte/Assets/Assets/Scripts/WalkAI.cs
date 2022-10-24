ff50eb1dd5d4fcb7975665495ee4fcf46662028f
KurtovBlin <90754795+KurtovBlin@users.noreply.github.com>
2022-10-24 11:47:50 +0100

    Ai Base

--- Big Cheese Donte/Assets/Assets/Scripts/WalkAI.cs
+++ Big Cheese Donte/Assets/Assets/Scripts/WalkAI.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WalkAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    NavMeshAgent agent;
+    
+    public Transform[] waypoints;
+    int waypointindex;
+    Vector3 target;
+
     void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDes();
+    }
+
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target)< 1)
+        {
+            ITWwaypointIndex();
+            UpdateDes();
+        }
+    }
+    void UpdateDes()
+    {
+        target = waypoints[waypointindex].position;
+        agent.SetDestination(target);
+    }
+
+    void ITWwaypointIndex()
+    {
+        waypointindex++;
+        if (waypointindex== waypoints.Length)
+        {
+            waypointindex = 0;
+        }
+    }
+    void Interacttimer()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public bool interact(bool a)
     {
-        
+
     }
 }
