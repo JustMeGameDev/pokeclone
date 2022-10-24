9c5c5f3c246a8c1102f8d0af72ced205db9265f2
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2022-10-24 09:34:58 +0200

    settings menu done

---
+++ Big Cheese Donte/Assets/Assets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform Target;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
