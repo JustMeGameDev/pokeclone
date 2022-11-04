3122f34011bfbed8369fb9f8811defedcfe565a9
KurtovBlin <90754795+KurtovBlin@users.noreply.github.com>
2022-11-04 13:52:33 +0100

    MergePush

--- Big Cheese Donte/Assets/Assets/Scripts/Player baseus/CamHolder.cs
+++ Big Cheese Donte/Assets/Assets/Scripts/Player baseus/CamHolder.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class CamHolder : MonoBehaviour
 {
     public Transform cameraPos;
+    public float CAMy;
+    public float CAMZ;
+    private static Quaternion Change(float x, float y, float z)
+    {
+        Quaternion newQuaternion = new Quaternion();
+        newQuaternion.Set(x, y, z, 1);
+        return newQuaternion;
+    }
     void Update()
     {
+        CAMy = cameraPos.transform.rotation.y;
+        CAMZ = cameraPos.transform.rotation.z;
         transform.position = cameraPos.position;
+        transform.rotation = Change(0, CAMy, 0);
     }
 }
