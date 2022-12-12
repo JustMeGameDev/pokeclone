763a7cd9d5122ed1f215f5174682ae5a4be510d4
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2022-12-12 15:13:52 +0100

    het begin

---
+++ Big Cheese Donte/Assets/script/invertory systeem/invertory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class invertory : MonoBehaviour
+{
+    string[] TeamMate;
+
+    string Items;
+
+   public void Invertory()
+    {
+
+    }
+}
