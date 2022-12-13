6d0d6086c695cbd297716c37415fe70e5bc39d2b
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2022-12-13 14:06:48 +0100

    dg

--- Big Cheese Donte/Assets/script/invertory systeem/invertory.cs
+++ Big Cheese Donte/Assets/script/invertory systeem/invertory.cs
@@ -5,12 +5,53 @@ using UnityEngine.UI;
 
 public class invertory : MonoBehaviour
 {
-    string[] TeamMate;
 
-    string Items;
+    string[] TeamMate = {"teammate 1","teammate 2","teammate 3","teamate 4","teammate 5","teammate 6"};
 
-   public void Invertory()
+    // The inventory is represented as an array of items
+    public GameObject[] ItemArray;
+
+    // The maximum number of items that can be held in the inventory
+    public int maxItems = 10;
+
+    void Start()
     {
+        // Initialize the array of items with the maximum number of items
+        ItemArray = new GameObject[maxItems];
+    }
 
+    // Adds an item to the inventory
+    public void AddItem(GameObject itemToAdd)
+    {
+        for (int i = 0; i < ItemArray.Length; i++)
+        {
+            if (ItemArray[i] == null)
+            {
+                // If there is an empty slot in the inventory, add the item
+                ItemArray[i] = itemToAdd;
+                return;
+            }
+        }
+    }
+
+    // Removes an item from the inventory
+    public void RemoveItem(GameObject itemToRemove)
+    {
+        for (int i = 0; i < ItemArray.Length; i++)
+        {
+            if (ItemArray[i] == itemToRemove)
+            {
+                // If the item is in the inventory, remove it
+                ItemArray[i] = null;
+                return;
+            }
+        }
     }
 }
+
+   
+
+
+
+
+
---
+++ Big Cheese Donte/Assets/script/invertory systeem/teammateRT.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teammateRT : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    List<GameObject> TeamMate = new List<GameObject>();
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
