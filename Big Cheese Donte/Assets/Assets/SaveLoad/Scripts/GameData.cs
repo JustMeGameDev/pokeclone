3b5639efd414e520c8d82dc9aaf2ac0dbc13c144
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2023-01-11 09:29:08 +0100

    save update

---
+++ Big Cheese Donte/Assets/Assets/SaveLoad/Scripts/GameData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public Vector3[] vector3s;
+    public KeyValuePair<string, int>[] ints;
+    private void Awake()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Big Cheese Donte/Assets/Assets/SaveLoad/Scripts/SaveHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveHandler : MonoBehaviour {
+
+    [SerializeField] private GameObject unitGameObject;
+    private GameData gameData;
+
+    private void Awake() {
+        gameData = GameObject.FindGameObjectWithTag("DataHandler").GetComponent<GameData>();
+        
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            Save();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            Load();
+        }
+    }
+
+    private void Save() {
+        // Save
+        
+
+        SaveObject saveObject = new SaveObject { 
+           
+        };
+        string json = JsonUtility.ToJson(saveObject);
+        SaveSystem.Save(json);
+
+        print("Saved!");
+    }
+
+    private void Load() {
+        // Load
+        string saveString = SaveSystem.Load();
+        if (saveString != null) {
+            print("Loaded: " + saveString);
+
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+
+           
+        } else {
+            print("No save");
+        }
+    }
+
+
+    private class SaveObject {
+        public int goldAmount;
+        public Vector3 playerPosition;
+    }
+}
