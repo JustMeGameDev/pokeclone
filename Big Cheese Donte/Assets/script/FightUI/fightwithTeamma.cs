b395250c6a90033fa9e8707ce329603fb85190e5
Dakkaoui050 <80848743+Dakkaoui050@users.noreply.github.com>
2023-01-18 14:40:57 +0100

    stapje verder

--- Big Cheese Donte/Assets/script/FightUI/fightwithTeamma.cs
+++ Big Cheese Donte/Assets/script/FightUI/fightwithTeamma.cs
@@ -7,10 +7,12 @@ public class fightwithTeamma : MonoBehaviour
 {
     //the teamMate member need to be showcase
     public GameObject[] team;
-    
+
+    BattleSystem BS;
 
     public GameObject ScriptUI;
     recruitment rec;
+    public GameObject scrollview;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +21,50 @@ public class fightwithTeamma : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        GameObject currentFound = rec.TeamMember[0];
+        GameObject currentFound = rec.TeamMember[rec.teamMember.Count -1];
         SpriteRenderer currentImage = currentFound.GetComponent<SpriteRenderer>();
         team[0].GetComponent<Image>().sprite = currentImage.sprite;
         team[1].GetComponent<Image>().sprite = currentImage.sprite;
+        team[2].GetComponent<Image>().sprite = currentImage.sprite;
         team[3].GetComponent<Image>().sprite = currentImage.sprite;
-        team[4].GetComponent<Image>().sprite = currentImage.sprite;
 
-        //for ()
-        //{
+       
+        
+    }
 
-        //}
+    public void TeamMateInZetten()
+    {
+        print(team.Length);
+        GameObject playerGo = Instantiate( team[team.Length]);
+        BS.playerunit = playerGo.GetComponent<Unit>();
+
+        if (scrollview)
+        { 
+            if (Input.GetMouseButtonDown(0))
+            {
+               inzetten();
+               StartCoroutine("inzetten");
+
+            }
+
+        }
+       
+
+        
+       
+        
+    }
+
+    public IEnumerator inzetten()
+    {  
+        BS.playerprefab.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+
+        team[1].SetActive(true);
 
     }
+    
 }
